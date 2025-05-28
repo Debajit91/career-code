07bb0a3ae053b6066ecf1b9374fc1922cbf92986
Debajit91 <debajitroy544@gmail.com>
2025-05-28 22:11:50 +0600

    job application form created

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React from "react";
+import { Link, useParams } from "react-router";
 
-import useAuth from '../../Hooks/useAuth';
+import useAuth from "../../Hooks/useAuth";
 
 const JobApply = () => {
-    const {id} = useParams();
-    const {user} = useAuth();
-    console.log(id, user);
-    return (
-        <div>
-            <h3 className="-text-4xl">Apply Job for</h3>
-        </div>
-    );
+  const { id } = useParams();
+  const { user } = useAuth();
+  
+  const handleApplyFormSubmit = e =>{
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
+    const gitHub = form.gitHub.value;
+    const resume = form.resume.value;
+  }
+  return (
+    <div className="max-w-lg mx-auto">
+      <h3 className="text-4xl text-center">Apply For This Job:<Link to={`/jobs/${id}`}>Details</Link> </h3>
+      <form onSubmit={handleApplyFormSubmit}>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+          
+
+          <label className="label">LinkedIn Profile</label>
+          <input type="url" className="input w-full" name="linkedIn" placeholder="LinkedIn Profile Link" />
+
+          <label className="label">GitHub Profile</label>
+          <input type="url" className="input w-full" name="gitHub" placeholder="GitHub Profile Link" />
+
+          <label className="label">Resume Link</label>
+          <input type="url" className="input w-full" name="resume" placeholder="Resume Link" />
+
+          <input type="submit" className="btn" value="Apply" />
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default JobApply;
