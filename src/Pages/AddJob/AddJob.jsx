f86fbcd6b3b073f89ab08eb41271d7f610b209f2
Debajit91 <debajitroy544@gmail.com>
2025-06-01 00:33:19 +0600

    add job form created & form data extracted

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import useAuth from "../../Hooks/useAuth";
 
 const AddJob = () => {
+    const {user} = useAuth();
+    const handleAddJob = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+
+        // Process salary range data
+        const {min, max , currency, ...newJob} = data;
+        newJob.salaryRange = {
+            min, max, currency
+        }
+
+        // Process requirements
+        // Way -1
+        // const requirementsString = newJob.requirements;
+        // const requirementsDirty = requirementsString.split(',');
+        // const requirementsClean = requirementsDirty.map(req => req.trim());
+        // newJob.requirements = requirementsClean;
+
+        // Way -2
+        // const requirements = newJob.requirements.split(',').map(req => req.trim());
+        // newJob.requirements = requirements;
+
+        // Way -3
+        newJob.requirements = newJob.requirements.split(',').map(req => req.trim());
+        console.log(newJob);
+    }
   return (
-    <div>
-      <h2>Please Add a Job</h2>
-      <form>
+    <div className="mx-auto w-xs">
+      <h2 className="text-center text-2xl">Please Add a Job</h2>
+      <form onSubmit={handleAddJob}>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
           <legend className="fieldset-legend">Basic Info</legend>
 
@@ -54,20 +83,18 @@ const AddJob = () => {
             <input
               className="btn"
               type="radio"
-              name="jobType"
+              name="jobType" value='On-Site'
               aria-label="On Site"
             />
             <input
               className="btn"
               type="radio"
-              name="jobType"
-              aria-label="Remote"
+              name="jobType" value='Remote'    aria-label="Remote"
             />
             <input
               className="btn"
               type="radio"
-              name="jobType"
-              aria-label="Hybrid"
+              name="jobType" value='Hybrid'       aria-label="Hybrid"
             />
           </div>
         </fieldset>
@@ -101,7 +128,7 @@ const AddJob = () => {
               <label className="label">Minimum Salary</label>
               <input
                 type="text"
-                name="salaryMin"
+                name="min"
                 className="input"
                 placeholder="Minimum Salary"
               />
@@ -111,7 +138,7 @@ const AddJob = () => {
               <label className="label">Maximum Salary</label>
               <input
                 type="text"
-                name="salaryMax"
+                name="max"
                 className="input"
                 placeholder="Maximum Salary"
               />
@@ -121,7 +148,7 @@ const AddJob = () => {
               <label className="label">Currency</label>
               <select
                 defaultValue="Select A Currency"
-                name="category"
+                name="currency"
                 className="select"
               >
                 <option disabled={true}>Select A Currency</option>
@@ -133,14 +160,56 @@ const AddJob = () => {
           </div>
         </fieldset>
 
-        {/*  */}
+        {/* Job Description */}
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          <legend className="fieldset-legend">Job Type</legend>
+          <legend className="fieldset-legend">Job Description</legend>
+          <textarea
+            name="description"
+            className="textarea"
+            placeholder="Job Description"
+          ></textarea>
         </fieldset>
 
-        {/*  */}
+        {/* Job Requirements */}
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          <legend className="fieldset-legend">Job Type</legend>
+          <legend className="fieldset-legend">Job Requirements</legend>
+          <textarea
+            name="requirements"
+            className="textarea"
+            placeholder="Requirements (separate by comma)"
+          ></textarea>
+        </fieldset>
+
+        {/* Job Responsibilities */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Job Responsibilities</legend>
+          <textarea
+            name="responsibilities"
+            className="textarea"
+            placeholder="Responsibilities (separate by comma)"
+          ></textarea>
+        </fieldset>
+
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">HR Info</legend>
+
+          <label className="label">HR Name</label>
+          <input
+            type="text"
+            name="hr_name"
+            className="input"
+            placeholder="HR Name"
+          />
+
+          <label className="label">HR Email</label>
+          <input
+            type="email"
+            name="hr_email" defaultValue={user.email} readOnly
+            className="input"
+            placeholder="HR Email"
+          />
+
+            <input type="submit" className="btn" value="Add Job" />
         </fieldset>
       </form>
     </div>
